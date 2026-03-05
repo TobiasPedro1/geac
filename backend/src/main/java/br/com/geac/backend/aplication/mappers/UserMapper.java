a33821e6a714577eb243854b9437ea2a532b5187
Dimas Neto <dimasneto2015@gmail.com>
2026-03-05 20:48:54 -0300

    fix: wrong event mapping property

--- backend/src/main/java/br/com/geac/backend/aplication/mappers/UserMapper.java
+++ backend/src/main/java/br/com/geac/backend/aplication/mappers/UserMapper.java
@@ -13,7 +13,7 @@ public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     @Mapping(target = "password", ignore = true)
-    @Mapping(target = "created_at", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "authorities", ignore = true)
     User registerToUser(RegisterRequestDTO registerRequestDTO);
